2a36f34fe202c282e8f1601ae0b5ad9b1d2d3e73
nujabescode <nujabescode@gmail.com>
2026-01-26 17:35:06 +0900

    슬라이더 작업

---
+++ src/components/contents/Anime.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { AnimeText } from '../../data/anime'
+import { Link } from 'react-router-dom'
+
+const Anime = () => {
+  return (
+      <section id='anime'>
+        <h2>😍추천 애니를 소개합니다.</h2>
+        <div className='video__inner'> 
+          {AnimeText.map((anime, key) => (
+            <div className="video" key={key}>
+                <div className="video__thumb play__icon">
+
+                    {/* /video/HjP_yAa2E1c */}
+                    <Link to={`/video/${anime.videoId}`}>
+                      <img src={anime.img} alt= {anime.title}/>
+                    </Link>
+                </div>
+            </div>
+
+          ))}
+
+        </div>
+      </section>
+  )
+}
+
+export default Anime
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Main from '../components/section/Main'
+import Today from '../components/contents/Today'
+import Music from '../components/contents/Music'
+import VideoSlider from '../components/video/VideoSlider'
+import { movieText } from '../data/movie'
+import { BookText } from '../data/book'
+import { AnimeText} from '../data/anime'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Main>
+      <Today/>
+      <Music/>
+      <VideoSlider videos={movieText} title='😍 추천 영화입니다' id='movie'/>
+      <VideoSlider videos={BookText} title='😎 추천 도서입니다' id='book'/>
+      <VideoSlider videos={AnimeText} title='😊 추천 애니입니다' id='anime'/>
+    </Main>
   )
 }
 
